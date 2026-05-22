7849a08b0bb78eec2b39734d07db172c53a69866
Paul Gaulmin <paul.gaulmin@ynov.com>
2026-05-22 08:38:23 +0200

    bdd: insertion et migration

--- backend/DB/migration.sql
+++ backend/DB/migration.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS forumDb CHARACTER SET utf8mb4 COLLATE utf8mb4_unic
                username VARCHAR(50) NOT NULL UNIQUE,
                email VARCHAR(100) NOT NULL UNIQUE,
                password VARCHAR(255) NOT NULL,
-               role ENUM('user', 'admin') DEFAULT 'user',
+               role ENUM('user', 'admin') DEFAULT 'user' NOT NULL,
                is_banned BOOLEAN,
                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
            );
